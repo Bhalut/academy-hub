a605c7cdd3aa756d9beae1bf5c3b0b048fb69513
Abdel Mejia <28445496+Bhalut@users.noreply.github.com>
2025-02-05 16:50:10 -0500

    feat: add datetime serialization for event data
    Implemented a `serialize_event_data` function in `event_processor.py` to handle serialization of `datetime` objects within event data. This ensures compatibility when processing and storing events.
    
    Changes:
    - Added `serialize_event_data` to recursively convert `datetime` objects to ISO 8601 string format in both nested dictionaries and lists.
    - Integrated serialization into the `process_event` function to ensure all incoming event data is pre-processed before further handling.
    - Improved data consistency for event storage and messaging systems like Kafka and MongoDB.
    
    This update enhances the reliability of event processing, especially when dealing with time-based data.

--- services/tracking/src/application/services/event_processor.py
+++ services/tracking/src/application/services/event_processor.py
@@ -1,3 +1,5 @@
+import datetime
+
 from shared.logger import log_error, log_event
 from ...config.tasks import process_event_data
 from ...infrastructure.messaging.kafka_producer import kafka_manager
@@ -6,8 +8,20 @@ from ...infrastructure.persistence.mongo_repository import MongoRepository
 repo = MongoRepository()
 
 
+def serialize_event_data(event_data):
+    if isinstance(event_data, dict):
+        return {key: serialize_event_data(value) for key, value in event_data.items()}
+    elif isinstance(event_data, list):
+        return [serialize_event_data(item) for item in event_data]
+    elif isinstance(event_data, datetime.datetime):
+        return event_data.isoformat()
+    return event_data
+
+
 async def process_event(event_data: dict):
     try:
+        event_data = serialize_event_data(event_data)
+
         event_type = event_data.get("event_type")
         if not event_type:
             log_error("Invalid event: missing event_type")
